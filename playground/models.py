ccb160f2318888d6ce9c60b916c70c7ba44d236e
Daniel Tefera Deti <danieltefera2211@gmail.com>
2022-04-15 10:56:17 +0300

    Test

--- playground/models.py
+++ playground/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-# Create your models here.
-class Tweets_table(models.Model):
+# Create your models here. Thest
     tweet_id = models.CharField(max_length=200)
     tweet_content = models.CharField(max_length=500)
     tweet_location = models.CharField(max_length=500)
