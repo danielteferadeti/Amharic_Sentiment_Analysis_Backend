90d932e46a5e717960fdefcc197060872f8d574f
Daniel Tefera Deti <danieltefera2211@gmail.com>
2022-04-15 10:57:24 +0300

    First Commit

--- playground/models.py
+++ playground/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here. Thest
+# Create your models here.
     tweet_id = models.CharField(max_length=200)
     tweet_content = models.CharField(max_length=500)
     tweet_location = models.CharField(max_length=500)
