fdaac713aacc5214ed1a49ad08828845c171792a
Daniel Tefera Deti <danieltefera2211@gmail.com>
2022-05-30 00:07:23 +0300

    final commit

--- Amharic_Sentiment_Analysis/settings.py
+++ Amharic_Sentiment_Analysis/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
-import django_heroku
 
 from pathlib import Path
 
@@ -139,11 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static"
-django_heroku.settings(locals())
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
